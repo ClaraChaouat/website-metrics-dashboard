e6a8b82c2f3f160fe46d1669dcaf9c3fda3432e8
Clara Chaouat <clarachaouat@Claras-MacBook-Pro.local>
2025-04-01 18:01:53 +0300

    removed global style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,7 +15,7 @@ const Card = ({ title, value, ariaLabel, onHover, onLeave }: CardProps) => {
             onBlur={onLeave}
             onMouseEnter={onHover}
             onMouseLeave={onLeave}
-            className="bg-white p-4 rounded shadow text-center hover:shadow-lg transition cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-300"
+            className="bg-white p-8 rounded shadow text-center hover:shadow-lg transition cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-300"
         >
             <h2 className="text-gray-500 text-sm mb-1">{title}</h2>
             <p className="text-xl font-semibold">{value}</p>
